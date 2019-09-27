3e461f9241fb8ab44f4ef04b014ec49778de7969
Amin Hasanov <a.hasanov@egov.local>
2019-09-27 12:15:14 +0400

    first commit

---
+++ src/app/table/tableitem.ts
@@ -0,0 +1,11 @@
+export class Tableitem {
+  number: number;
+  rname: string;
+  description: string;
+  date: number;
+  starttime: number;
+  endtime: number;
+  status: string;
+  editimg: string;
+  deleteimg: string;
+}
